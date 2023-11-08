8e16a99d576c098f8cdf61a2eb24eb07b915cd69
Quiomachado <quiomachado@gmail.com>
2023-11-08 21:31:03 +0000

    Revert "Revert "day1 in rust""
    This reverts commit df5ffd2f3cb672ea540bfc1f9f19c43b4ef82ebf.

---
+++ rust/day1/target/debug/deps/rand_chacha-912b6b324c60fd6a.d
@@ -0,0 +1,7 @@
+/home/quiomachado/Documents/FreeCoding/adventOfCode2022/rust/day1/target/debug/deps/rand_chacha-912b6b324c60fd6a.rmeta: /home/quiomachado/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/quiomachado/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/quiomachado/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
+
+/home/quiomachado/Documents/FreeCoding/adventOfCode2022/rust/day1/target/debug/deps/rand_chacha-912b6b324c60fd6a.d: /home/quiomachado/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/quiomachado/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/quiomachado/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
+
+/home/quiomachado/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs:
+/home/quiomachado/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs:
+/home/quiomachado/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs:
